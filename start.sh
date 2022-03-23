659b8b5031c719fcdf0205ed59f15c2a4344f1a2
kuthank22 <99564997+kuthank22@users.noreply.github.com>
2022-03-23 12:48:33 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # configs
-AUUID=UUID
+AUUID=a632c737-23ce-4196-af93-78e666f355f7
 CADDYIndexPage=https://github.com/PavelDoGreat/WebGL-Fluid-Simulation/archive/master.zip
-CONFIGCADDY=https://raw.githubusercontent.com/账号名/仓库名/master/etc/Caddyfile
-CONFIGXRAY=https://raw.githubusercontent.com/账号名/仓库名/master/etc/xray.json
+CONFIGCADDY=https://raw.githubusercontent.com/kuthank22/ejector/master/etc/Caddyfile
+CONFIGXRAY=https://raw.githubusercontent.com/kuthank22/ejector/master/etc/xray.json
 ParameterSSENCYPT=chacha20-ietf-poly1305
-StoreFiles=https://raw.githubusercontent.com/账号名/仓库名/master/etc/StoreFiles
+StoreFiles=https://raw.githubusercontent.com/kuthank22/ejector/master/etc/StoreFiles
 #PORT=4433
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
@@ -15,7 +15,7 @@ wget -qO- $CONFIGXRAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$P
 
 # storefiles
 mkdir -p /usr/share/caddy/$AUUID
-wget -P /usr/share/caddy/$AUUID 压缩包链接
+wget -P /usr/share/caddy/$AUUID https://github.com/kuthank22/xrocket/raw/main/rocket.zip
 
 
 for file in $(ls /usr/share/caddy/$AUUID); do
